ff4dca0243400858287ec7ef94879a02c7db3be3
ASCARLI <1658795301@qq.com>
2024-11-12 00:46:33 +0800

    Add files via upload

---
+++ AVL/BST.h
@@ -0,0 +1,461 @@
+#ifndef AVL_TREE_H
+#define AVL_TREE_H
+
+#include "dsexceptions.h"
+#include <algorithm>
+#include <iostream> 
+using namespace std;
+
+// BinarySearchTree class
+//
+// CONSTRUCTION: zero parameter
+//
+// ******************PUBLIC OPERATIONS*********************
+// void insert( x )       --> Insert x
+// void remove( x )       --> Remove x (unimplemented)
+// bool contains( x )     --> Return true if x is present
+// Comparable findMin( )  --> Return smallest item
+// Comparable findMax( )  --> Return largest item
+// boolean isEmpty( )     --> Return true if empty; else false
+// void makeEmpty( )      --> Remove all items
+// void printTree( )      --> Print tree in sorted order
+// ******************ERRORS********************************
+// Throws UnderflowException as warranted
+
+template <typename Comparable>
+class BinarySearchTree
+{
+  public:
+    BinarySearchTree( ) : root{ nullptr }
+      { }
+    
+    BinarySearchTree( const BinarySearchTree & rhs ) : root{ nullptr }
+    {
+        root = clone( rhs.root );
+    }
+
+    BinarySearchTree( BinarySearchTree && rhs ) : root{ rhs.root }
+    {
+        rhs.root = nullptr;
+    }
+    
+    ~BinarySearchTree( )
+    {
+        makeEmpty( );
+    }
+
+    /**
+     * Deep copy.
+     */
+    BinarySearchTree & operator=( const BinarySearchTree & rhs )
+    {
+        BinarySearchTree copy = rhs;
+        std::swap( *this, copy );
+        return *this;
+    }
+        
+    /**
+     * Move.
+     */
+    BinarySearchTree & operator=( BinarySearchTree && rhs )
+    {
+        std::swap( root, rhs.root );
+        
+        return *this;
+    }
+    
+    /**
+     * Find the smallest item in the tree.
+     * Throw UnderflowException if empty.
+     */
+    const Comparable & findMin( ) const
+    {
+        if( isEmpty( ) )
+            throw UnderflowException{ };
+        return findMin( root )->element;
+    }
+
+    /**
+     * Find the largest item in the tree.
+     * Throw UnderflowException if empty.
+     */
+    const Comparable & findMax( ) const
+    {
+        if( isEmpty( ) )
+            throw UnderflowException{ };
+        return findMax( root )->element;
+    }
+
+    /**
+     * Returns true if x is found in the tree.
+     */
+    bool contains( const Comparable & x ) const
+    {
+        return contains( x, root );
+    }
+
+    /**
+     * Test if the tree is logically empty.
+     * Return true if empty, false otherwise.
+     */
+    bool isEmpty( ) const
+    {
+        return root == nullptr;
+    }
+
+    /**
+     * Print the tree contents in sorted order.
+     */
+    void printTree( ) const
+    {
+        if( isEmpty( ) )
+            cout << "Empty tree" << endl;
+        else
+            printTree( root );
+    }
+
+    /**
+     * Make the tree logically empty.
+     */
+    void makeEmpty( )
+    {
+        makeEmpty( root );
+    }
+
+    /**
+     * Insert x into the tree; duplicates are ignored.
+     */
+    void insert( const Comparable & x )
+    {
+        insert( x, root );
+    }
+     
+    /**
+     * Insert x into the tree; duplicates are ignored.
+     */
+    void insert( Comparable && x )
+    {
+        insert( std::move( x ), root );
+    }
+     
+    /**
+     * Remove x from the tree. Nothing is done if x is not found.
+     */
+    void remove( const Comparable & x )
+    {
+        remove( x, root );
+    }
+
+  protected:
+    struct BinaryNode
+    {
+        Comparable element;
+        BinaryNode   *left;
+        BinaryNode   *right;
+        int       height;
+
+        BinaryNode( const Comparable & ele, BinaryNode *lt, BinaryNode *rt, int h = 0 )
+          : element{ ele }, left{ lt }, right{ rt }, height{ h } { }
+        
+        BinaryNode( Comparable && ele, BinaryNode *lt, BinaryNode *rt, int h = 0 )
+          : element{ std::move( ele ) }, left{ lt }, right{ rt }, height{ h } { }
+    };
+
+    BinaryNode *root;
+
+
+    /**
+     * Internal method to insert into a subtree.
+     * x is the item to insert.
+     * t is the node that roots the subtree.
+     * Set the new root of the subtree.
+     */
+    void insert( const Comparable & x, BinaryNode * & t )
+    {
+        if( t == nullptr )
+            t = new BinaryNode{ x, nullptr, nullptr };
+        else if( x < t->element )
+            insert( x, t->left );
+        else if( t->element < x )
+            insert( x, t->right );
+        
+        balance( t );
+    }
+
+    /**
+     * Internal method to insert into a subtree.
+     * x is the item to insert.
+     * t is the node that roots the subtree.
+     * Set the new root of the subtree.
+     */
+    void insert( Comparable && x, BinaryNode * & t )
+    {
+        if( t == nullptr )
+            t = new BinaryNode{ std::move( x ), nullptr, nullptr };
+        else if( x < t->element )
+            insert( std::move( x ), t->left );
+        else if( t->element < x )
+            insert( std::move( x ), t->right );
+        
+        balance( t );
+    }
+     
+
+
+
+
+    /**
+     * Internal method to remove from a subtree.
+     * x is the item to remove.
+     * t is the node that roots the subtree.
+     * Set the new root of the subtree.
+     */
+    void remove( const Comparable & x, BinaryNode * & t )
+    {
+        if( t == nullptr )
+            return;   // Item not found; do nothing
+        if( x < t->element )
+            remove( x, t->left );
+        else if( t->element < x )
+            remove( x, t->right );
+        else if( t->left == nullptr || t->right == nullptr ) //只有一个孩子，或没有孩子
+        {
+            BinaryNode *oldNode = t;
+            t = ( t->left != nullptr ) ? t->left : t->right;
+            delete oldNode;
+        }
+        else // Two children
+        {
+            BinaryNode *noderemove = t;
+            BinaryNode *nodeDetachMin = detachMin(t->right);
+            nodeDetachMin->left = noderemove->left;
+            nodeDetachMin->right = noderemove->right;
+            t = nodeDetachMin;
+            delete noderemove;
+        }
+
+        balance( t );
+    }
+
+    // 查找以 t 为根的子树中的最小节点，返回这个节点，并从原子树中删除这个节点
+    BinaryNode *detachMin(BinaryNode *&t) { //t一定非空
+        BinaryNode *nodereturn = t;
+        if (t->left == nullptr) //特殊情况：只有一个根，或根只有右节点
+        {
+            t = t->right;
+            return nodereturn;
+        }
+        else
+        {
+            return detachMin(t->left);
+        }
+        
+        balance(t);
+
+
+        /*
+        
+        
+        //有左节点的情况
+        BinaryNode *next = nodereturn->left;
+        //下面nodereturn名字含义错误，实际上return的是next，但是就照着用了
+        while (nodereturn->left->left != nullptr) //直到nodereturn的左节点没有左节点了
+        {
+            nodereturn = nodereturn->left;
+        }
+        nodereturn->left = next->right;
+        return next;
+        
+        */
+        
+    }
+
+
+
+    static const int ALLOWED_IMBALANCE = 1;
+
+    // Assume t is balanced or within one of being balanced
+    void balance( BinaryNode * & t )
+    {
+        if( t == nullptr )
+            return;
+        
+        if( height( t->left ) - height( t->right ) > ALLOWED_IMBALANCE )
+            if( height( t->left->left ) >= height( t->left->right ) )
+                rotateWithLeftChild( t );
+            else
+                doubleWithLeftChild( t );
+        else
+        if( height( t->right ) - height( t->left ) > ALLOWED_IMBALANCE )
+            if( height( t->right->right ) >= height( t->right->left ) )
+                rotateWithRightChild( t );
+            else
+                doubleWithRightChild( t );
+                
+        t->height = max( height( t->left ), height( t->right ) ) + 1;
+    }
+    
+    /**
+     * Internal method to find the smallest item in a subtree t.
+     * Return node containing the smallest item.
+     */
+    BinaryNode * findMin( BinaryNode *t ) const
+    {
+        if( t == nullptr )
+            return nullptr;
+        if( t->left == nullptr )
+            return t;
+        return findMin( t->left );
+    }
+
+    /**
+     * Internal method to find the largest item in a subtree t.
+     * Return node containing the largest item.
+     */
+    BinaryNode * findMax( BinaryNode *t ) const
+    {
+        if( t != nullptr )
+            while( t->right != nullptr )
+                t = t->right;
+        return t;
+    }
+
+
+    /**
+     * Internal method to test if an item is in a subtree.
+     * x is item to search for.
+     * t is the node that roots the tree.
+     */
+    bool contains( const Comparable & x, BinaryNode *t ) const
+    {
+        if( t == nullptr )
+            return false;
+        else if( x < t->element )
+            return contains( x, t->left );
+        else if( t->element < x )
+            return contains( x, t->right );
+        else
+            return true;    // Match
+    }
+/****** NONRECURSIVE VERSION*************************
+    bool contains( const Comparable & x, BinaryNode *t ) const
+    {
+        while( t != nullptr )
+            if( x < t->element )
+                t = t->left;
+            else if( t->element < x )
+                t = t->right;
+            else
+                return true;    // Match
+
+        return false;   // No match
+    }
+*****************************************************/
+
+    /**
+     * Internal method to make subtree empty.
+     */
+    void makeEmpty( BinaryNode * & t )
+    {
+        if( t != nullptr )
+        {
+            makeEmpty( t->left );
+            makeEmpty( t->right );
+            delete t;
+        }
+        t = nullptr;
+    }
+
+    /**
+     * Internal method to print a subtree rooted at t in sorted order.
+     */
+    void printTree( BinaryNode *t ) const
+    {
+        if( t != nullptr )
+        {
+            printTree( t->left );
+            cout << t->element << endl;
+            printTree( t->right );
+        }
+    }
+
+    /**
+     * Internal method to clone subtree.
+     */
+    BinaryNode * clone( BinaryNode *t ) const
+    {
+        if( t == nullptr )
+            return nullptr;
+        else
+            return new BinaryNode{ t->element, clone( t->left ), clone( t->right ), t->height };
+    }
+        // Avl manipulations
+    /**
+     * Return the height of node t or -1 if nullptr.
+     */
+    int height( BinaryNode *t ) const
+    {
+        return t == nullptr ? -1 : t->height;
+    }
+
+    int max( int lhs, int rhs ) const
+    {
+        return lhs > rhs ? lhs : rhs;
+    }
+
+    /**
+     * Rotate binary tree node with left child.
+     * For AVL trees, this is a single rotation for case 1.
+     * Update heights, then set new root.
+     */
+    void rotateWithLeftChild( BinaryNode * & k2 )
+    {
+        BinaryNode *k1 = k2->left;
+        k2->left = k1->right;
+        k1->right = k2;
+        k2->height = max( height( k2->left ), height( k2->right ) ) + 1;
+        k1->height = max( height( k1->left ), k2->height ) + 1;
+        k2 = k1;
+    }
+
+    /**
+     * Rotate binary tree node with right child.
+     * For AVL trees, this is a single rotation for case 4.
+     * Update heights, then set new root.
+     */
+    void rotateWithRightChild( BinaryNode * & k1 )
+    {
+        BinaryNode *k2 = k1->right;
+        k1->right = k2->left;
+        k2->left = k1;
+        k1->height = max( height( k1->left ), height( k1->right ) ) + 1;
+        k2->height = max( height( k2->right ), k1->height ) + 1;
+        k1 = k2;
+    }
+
+    /**
+     * Double rotate binary tree node: first left child.
+     * with its right child; then node k3 with new left child.
+     * For AVL trees, this is a double rotation for case 2.
+     * Update heights, then set new root.
+     */
+    void doubleWithLeftChild( BinaryNode * & k3 )
+    {
+        rotateWithRightChild( k3->left );
+        rotateWithLeftChild( k3 );
+    }
+
+    /**
+     * Double rotate binary tree node: first right child.
+     * with its left child; then node k1 with new right child.
+     * For AVL trees, this is a double rotation for case 3.
+     * Update heights, then set new root.
+     */
+    void doubleWithRightChild( BinaryNode * & k1 )
+    {
+        rotateWithLeftChild( k1->right );
+        rotateWithRightChild( k1 );
+    }
+};
+
+#endif
